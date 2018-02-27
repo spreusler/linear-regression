fc2528f5e94bf9ac57609064b675cdc1c2cab8e2
Stefan Preusler <stefan@preusler.de>
2018-02-27 16:34:41 +0100

    Code & Quelldaten

--- linear-regression.py
+++ linear-regression.py
@@ -3,8 +3,28 @@
 
 # In[ ]:
 
-
+# Datenstruktur Library
 import pandas as pd
+# Machine Learning Library
 from sklearn import linear_model
+# Plotting Library
 import matplotlib.pyplot as plt
 
+# Daten einlesen
+dataframe = pd.read_fwf('brain_body.txt')
+# Zuweisung von Spalten
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+
+# Training des Modells
+# Erzeugung Lineare Regression
+body_reg = linear_model.LinearRegression()
+# Übergabe der Daten für Training
+body_reg.fit(x_values, y_values)
+
+# Visualisierung der Daten
+# Punkte des Datensatzes
+plt.scatter(x_values, y_values)
+# Vorhersage Graph
+plt.plot(x_values, body_reg.predict(x_values))
+plt.show()
